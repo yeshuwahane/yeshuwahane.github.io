3017eb6a21c8998c066acf72653d21c266181f41
yeshuwahane <yeshuwahane@gmail.com>
2020-09-19 16:49:45 +0530

    modifyied Geometric Pattern

--- Relax/Relac.js
+++ Relax/Relac.js
@@ -1,36 +1,34 @@
 var Num_Circles = 12;
 var circleDiameter;
+var circleRadius;
 
 function setup (){
  createCanvas(480, 600);
  circleDiameter = width/Num_Circles;
+ circleRadius = circleDiameter/2;
+
 }
 
 function draw(){
- // ellipse(width/2,height/2,circleDiameter,circleDiameter);
- // ellipse(0, height/2, circleDiameter, circleDiameter);
- // ellipse(circleDiameter,height/2, circleDiameter, circleDiameter);
- // ellipse(2 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(3 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(4 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(5 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(6 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(7 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(8 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(9 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(10 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(11 * circleDiameter, height/2, circleDiameter, circleDiameter);
- // ellipse(12 * circleDiameter, height/2, circleDiameter, circleDiameter);
 
+    var isShifted = false;
     var y = 0;
     while (y <= height) {
 
-        var x = 0;
+        var x;
+
+        if(isShifted){
+            x = circleRadius;
+        }
+        else {
+            x = 0;
+        }
         while (x <= width) {
             ellipse(x, y, circleDiameter, circleDiameter);
             x = x + circleDiameter;
         }
 
-        y = y + circleDiameter;
+        y = y + circleRadius;
+        isShifted = !isShifted;
     }
 }
